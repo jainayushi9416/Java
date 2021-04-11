5d07c50e854fc03b618e256b404aa05bb79a3647
ayushijain9 <52760097+jainayushi9416@users.noreply.github.com>
2021-04-11 19:18:27 +0530

    Add files via upload

---
+++ Tableofnumber.java
@@ -0,0 +1,16 @@
+//WAP to print table of a number n  till 10.
+import java.util.*;
+
+public class Tableofnumber {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+(n*i));
+        }
+
+        // write ur code here
+
+    }
+}
